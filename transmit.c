3b8ae7fd9f00a6fa4a0099b6163fb15d8c839724
hemanwld <hekachacha@gmail.com>
2023-06-30 17:02:56 +0800

    demo0_testfinish

--- transmit.c
+++ transmit.c
@@ -1,66 +1,66 @@
 #include "transmit.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 void transmit_send(unsigned char xdata *arr, unsigned char sizeofarr, unsigned char addr)
 {
-    extern send_Uart(unsigned char x);
+    extern void Send_UART(unsigned char a);
+
     static unsigned char i;
     static unsigned char head     = 0;
     static unsigned char tail_bak = 0;
 
     do {
         head = (unsigned char)rand();
+        rand();
     } while (head == tail_bak);
-    send_Uart(head);
-    send_Uart(head + 1);
-    send_Uart(head + 2);
+    Send_UART(head);
+    Send_UART(head + 1);
+    Send_UART(head + 2);
+    Send_UART(addr);
+    Send_UART(~addr);
     for (i = 0; i < sizeofarr; i++) {
-        send_Uart(arr[i]);
-        send_Uart(~arr[i]);
+        Send_UART(arr[i]);
+        Send_UART(~arr[i]);
         if (i == sizeofarr - 1) {
             tail_bak = ~arr[i];
         }
     }
 }
 
-bit transmit_receive(unsigned char xdata *arr, unsigned char sizeofarr, unsigned char addr)
+bit transmit_receive(unsigned char xdata *arr_receive, unsigned char sizeofarr_receive, unsigned char addr, unsigned char Buffer_Receive_Uart)
 {
-    static unsigned char xdata data_head[5] = {0};
-    static unsigned char xdata p_head = 0, p_data = 0;
+    static unsigned char xdata datahead_bak0 = 0, datahead_bak1 = 0, datahead_bak2 = 0;
+    static unsigned char xdata datahead0 = 0, datahead1 = 0;
+    static unsigned char xdata p_head = 0;
 
-    if (p_head < 5) {
-        data_head[p_head] = Buffer_Receive_Uart;
-        p_head++;
-        if (p_head >= 2 && p_head <= 3) {
-            if (data_head[p_head - 2] + 1 != data_head[p_head - 1]) p_head = 0;
-        } else if (p_head == 5) {
-            if ((data_head[3] != ~data_head[4]) || data_head[3] != addr) {
-                p_head = 0;
-            }
+    datahead_bak0 = datahead_bak1;
+    datahead_bak1 = datahead_bak2;
+    datahead_bak2 = Buffer_Receive_Uart;
+    //----------------------------------------------------------------
+    if (p_head == 1) {
+        datahead0 = Buffer_Receive_Uart;
+    } else if (p_head == 2) {
+        datahead1 = Buffer_Receive_Uart;
+        if (datahead0 != addr || datahead0 != ~datahead1) {
+            p_head = 0xff;
         }
     }
-    if (p_head == 5) {
-        if (p_data < sizeofarr) {
-            arr[p_data] = Buffer_Receive_Uart;
-            p_data++;
-            if (p_data % 2 == 0) {
-                if (arr[p_data - 1] != arr[p_data - 2]) {
-                    p_data = 0;
-                    p_head = 0;
-                    // return receive_notfinish;
-                }
-                if (p_data == sizeofarr) {
-                    p_data = 0;
-                    p_head = 0;
-                    return receive_finish;
-                }
-            }
-        } else {
-            p_data = 0;
-            p_head = 0;
-            // return receive_notfinish;
+    if (p_head >= 3 && p_head <= (sizeofarr_receive + 2)) {
+        arr_receive[p_head - 3] = Buffer_Receive_Uart;
+        if (p_head % 2 == 0) {
+            if (arr_receive[p_head - 3] != ~arr_receive[p_head - 4]) p_head = 0xff;
         }
     }
+    if (p_head == (sizeofarr_receive + 2)) {
+        p_head = 0xff;
+        return receive_finish;
+    }
+    //-----------------------------------------------------------------
+    if ((datahead_bak0 + 1 == datahead_bak1) && (datahead_bak1 + 1 == datahead_bak2)) {
+        p_head = 0;
+    }
+    if (p_head != 0xff) p_head++;
+
     return receive_notfinish;
 }
 
@@ -71,4 +71,6 @@ void transmit_get(unsigned char xdata *arr_receive, unsigned char xdata *arr_get
     for (i = 0; i < sizeofarr; i++) {
         arr_get[i] = arr_receive[2 * i];
     }
+    Send_UART(arr_get[0]);
+    Send_UART(arr_get[1]);
 }
--- transmit.h
+++ transmit.h
@@ -4,9 +4,9 @@
 #define receive_finish         1
 #define receive_notfinish      0
 
-#define Buffer_Receive_Uart   RI
-
-
+void transmit_send(unsigned char xdata *arr, unsigned char sizeofarr, unsigned char addr);
+bit transmit_receive(unsigned char xdata *arr, unsigned char sizeofarr, unsigned char addr,unsigned char Buffer_Receive_Uart);
+void transmit_get(unsigned char xdata *arr_receive, unsigned char xdata *arr_get, unsigned char sizeofarr);
 
 
 #endif
