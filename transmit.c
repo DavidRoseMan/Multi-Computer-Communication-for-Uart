4d8aacff894048b28eb42a83eac1e4a6c6988fe4
hemanwld <hekachacha@gmail.com>
2023-07-18 11:25:29 +0800

    加入了stc8h串口结构体初始化等等的函数

--- transmit.c
+++ transmit.c
@@ -1,40 +1,42 @@
 #include "transmit.h"
 #include <stdlib.h>
+#include "variable.h"
 
-void transmit_send(unsigned char xdata *arr, unsigned char sizeofarr, unsigned char addr)
+void transmit_send(Uart_type *example, unsigned char xdata *arr, \
+                   unsigned char sizeofarr, unsigned char addr)
 {
-    extern void Send_UART(unsigned char a);
+    extern void Send_UART(Uart_type * example, unsigned char Data);
 
-    static unsigned char i;
-    static unsigned char head     = 0;
-    static unsigned char tail_bak = 0;
+    unsigned char i;
+    unsigned char head = 0;
 
     do {
         head = (unsigned char)rand();
-    } while (head == tail_bak+2);
-    Send_UART(head);
-    Send_UART(head + 2);
-    Send_UART(head + 4);
-    Send_UART(addr);
-    Send_UART(~addr);
+    } while (head == example->tail_bak + 2);
+    Send_UART(example, head);
+    Send_UART(example, head + 2);
+    Send_UART(example, head + 4);
+    Send_UART(example, addr);
+    Send_UART(example, ~addr);
     for (i = 0; i < sizeofarr; i++) {
-        Send_UART(arr[i]);
-        Send_UART(~arr[i]);
+        Send_UART(example, arr[i]);
+        Send_UART(example, ~arr[i]);
         if (i == sizeofarr - 1) {
-            tail_bak = ~arr[i];
+            example->tail_bak = ~arr[i];
         }
     }
 }
 
-bit transmit_receive(unsigned char xdata *arr_receive, unsigned char sizeofarr_receive, unsigned char addr, unsigned char Buffer_Receive_Uart)
+bit transmit_receive(unsigned char xdata *arr_receive, unsigned char sizeofarr_receive, \
+                     unsigned char addr, unsigned char Buffer_Receive_Uart)
 {
-    static unsigned char xdata datahead_bak0 = 0, datahead_bak1 = 0, datahead_bak2 = 0;
+    static unsigned char xdata arr_datahead_bak[3] = {0};
     static unsigned char xdata datahead0 = 0, datahead1 = 0;
-    static unsigned char xdata ptt = 0xff;//point to tail
+    static unsigned char xdata ptt = 0xff; // point to tail
 
-    datahead_bak0 = datahead_bak1;
-    datahead_bak1 = datahead_bak2;
-    datahead_bak2 = Buffer_Receive_Uart;
+    arr_datahead_bak[0] = arr_datahead_bak[1];
+    arr_datahead_bak[1] = arr_datahead_bak[2];
+    arr_datahead_bak[2] = Buffer_Receive_Uart;
     //----------------------------------------------------------------
     if (ptt == 1) {
         datahead0 = Buffer_Receive_Uart;
@@ -55,7 +57,8 @@ bit transmit_receive(unsigned char xdata *arr_receive, unsigned char sizeofarr_r
         return receive_finish;
     }
     //-----------------------------------------------------------------
-    if ((datahead_bak0 + 2 == datahead_bak1) && (datahead_bak1 + 2 == datahead_bak2)) {
+    if ((arr_datahead_bak[0] + 2 == arr_datahead_bak[1]) &&
+        (arr_datahead_bak[1] + 2 == arr_datahead_bak[2])) {
         ptt = 0;
     }
     if (ptt != 0xff) ptt++;
@@ -69,5 +72,96 @@ void transmit_get(unsigned char xdata *arr_receive, unsigned char xdata *arr_get
 
     for (i = 0; i < sizeofarr_get; i++) {
         arr_get[i] = arr_receive[2 * i];
+    }
+}
+
+//--------------------------·¢ËÍ-------------------------------------------------------------
+
+void init_Uart(Uart_type *example, unsigned char *Buffer, unsigned char sizeofbuffer)
+{
+    example->i            = 0;
+    example->end          = 0;
+    example->Buffer       = Buffer;
+    example->sizeofBuffer = sizeofbuffer;
+    example->timer_100us  = 0;
+    example->tail_bak     = 0;
+}
+
+void Send_UART(Uart_type *example, unsigned char Data)
+{
+    unsigned char i;
+
+    i = example->end;
+    if (i >= example->i) {
+        if (i != example->sizeofBuffer - 1)
+            i++;
+        else {
+            while (example->i == 0)
+                ;
+            i = 0;
+        }
+    } else {
+        {
+            while (example->i == 1)
+                ;
+            i++;
+        }
+    }
+    example->Buffer[i] = Data;
+    example->end       = i;
+}
+
+void UART_Process_Send(Uart_type *example, unsigned char number_uart)
+{
+    if (example->timer_100us != 255)
+        example->timer_100us++;
+    else if (example->i != example->end) {
+        if (number_uart == 1)
+            SCON |= 0x02;
+        else if (number_uart == 2)
+            S2CON |= 0x02;
+        else if (number_uart == 3)
+            S3CON |= 0x02;
+        else if (number_uart == 4)
+            S4CON |= 0x02;
+    }
+}
+
+void UART_Process_Send_ITR(Uart_type *example, unsigned char number_uart)
+{
+    unsigned char a;
+
+    if (number_uart == 1)
+        a = SCON;
+    else if (number_uart == 2)
+        a = S2CON;
+    else if (number_uart == 3)
+        a = S3CON;
+    else if (number_uart == 4)
+        a = S4CON;
+
+    if (a & 0x02) {
+        if (number_uart == 1)
+            SCON &= ~0x02;
+        else if (number_uart == 2)
+            S2CON &= ~0x02;
+        else if (number_uart == 3)
+            S3CON &= ~0x02;
+        else if (number_uart == 4)
+            S4CON &= ~0x02;
+        example->timer_100us = 0;
+        if (example->i != example->end) {
+            example->i++;
+            if (number_uart == 1)
+                SBUF = example->Buffer[example->i];
+            else if (number_uart == 1)
+                S2BUF = example->Buffer[example->i];
+            else if (number_uart == 1)
+                S3BUF = example->Buffer[example->i];
+            else if (number_uart == 1)
+                S4BUF = example->Buffer[example->i];
+        } else {
+            example->timer_100us = 255;
+        }
     }
 }
--- transmit.h
+++ transmit.h
@@ -1,13 +1,28 @@
 #ifndef _transmit_h
 #define _transmit_h
 
-#define receive_finish         1
-#define receive_notfinish      0
+typedef xdata struct Uart_struct {
+    volatile unsigned char  i;
+    volatile unsigned char  end;
+    volatile unsigned char  *Buffer;
+    volatile unsigned char  sizeofBuffer;
+    volatile unsigned char  timer_100us;
+    volatile unsigned char  tail_bak;
+} Uart_type;
 
-void transmit_send(unsigned char xdata *arr, unsigned char sizeofarr, unsigned char addr);
-bit transmit_receive(unsigned char xdata *arr_receive, unsigned char sizeofarr_receive, unsigned char addr, unsigned char Buffer_Receive_Uart);
+#define receive_finish    1
+#define receive_notfinish 0
+
+void transmit_send(Uart_type *example, unsigned char xdata *arr, \
+                   unsigned char sizeofarr, unsigned char addr);
+bit transmit_receive(unsigned char xdata *arr_receive, unsigned char sizeofarr_receive, \
+                     unsigned char addr, unsigned char Buffer_Receive_Uart);
 void transmit_get(unsigned char xdata *arr_receive, unsigned char xdata *arr_get, unsigned char sizeofarr_get);
-
+//----------------------------------------------------------------------------------------
+void init_Uart(Uart_type *example, unsigned char *Buffer, unsigned char sizeofbuffer);
+void Send_UART(Uart_type *example, unsigned char Data);
+void UART_Process_Send(Uart_type *example, unsigned char number_uart);
+void UART_Process_Send_ITR(Uart_type *example, unsigned char number_uart);
 
 #endif
 
